9978a803acdcc857f45595b0877e22443ea788b9
BLAZE-GEC <aswinch101@gmail.com>
2024-07-02 23:08:04 -0700

    Update smscontroller.dart

--- lib/smscontroller.dart
+++ lib/smscontroller.dart
@@ -1,36 +1,26 @@
-
-import 'dart:isolate';
-import 'dart:ui';
-
 import 'package:flutter/widgets.dart';
-import 'package:flutter_notification_listener/flutter_notification_listener.dart';
 import 'package:get/get.dart';
 import 'package:notification_listener_service/notification_event.dart';
 import 'package:notification_listener_service/notification_listener_service.dart';
-get notificationList => null;
 
 class SMSController extends GetxController {
-
-  RxList notificationList = RxList<ServiceNotificationEvent>();
+  var notificationList = <ServiceNotificationEvent>[].obs;
 
   @override
   void onInit() {
-    // TODO: implement $configureLifeCycle
     super.onInit();
     listenNotification();
   }
 
   void requestForPermission() async {
-    print("requesting perm");
+    print("requesting permission");
     final bool status = await NotificationListenerService.isPermissionGranted();
-    if (status != true) {
+    if (!status) {
       print("no permission");
-      final bool statuss =
       await NotificationListenerService.requestPermission();
     }
     listenNotification();
   }
-
 
   void listenNotification() {
     print("Listening for notifications...");
@@ -39,7 +29,9 @@ class SMSController extends GetxController {
       print("Current notification: $event");
 
       if (event.packageName == "com.whatsapp") {
-        notificationList.add(event);
+        if (!notificationList.any((e) => e.id == event.id && e.content == event.content)) {
+          notificationList.add(event);
+        }
       }
     });
   }
